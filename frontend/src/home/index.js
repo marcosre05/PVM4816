0f4508b362347561a0bfe137e68e49bb7ebecd61
Marcos Reales <marrealan@alum.us.es>
2025-11-11 13:38:12 +0100

    Añadir las fotos/logos a la página de inicio. Fixes #5

--- frontend/src/home/index.js
+++ frontend/src/home/index.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import '../App.css';
 import '../static/css/home/home.css'; 
+import logo from '../static/images/usLogoPNG.png';
+
 
 export default function Home(){
     return(
         <div className="home-page-container">
             <div className="hero-div">
                 <h1>Your game</h1>
+                <img src={logo}/>
                 <h3>---</h3>
                 <h3>Do you want to play?</h3>                
             </div>
